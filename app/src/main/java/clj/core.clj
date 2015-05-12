af9135604a6ca683698bd8cce0da59e6f9a9271c
noncom <alex.murataev@gmail.com>
2015-05-12 19:24:45 +0400

    + fully working empty demo. you can start making you app right away

--- app/src/main/java/clj/core.clj
+++ app/src/main/java/clj/core.clj
@@ -1,7 +1,12 @@
 (ns clj.core
-  (:require [clj.input :as input]))
+  (:require [clj.input :as input]
+			[clj.state :as state]))
 
 (defn hello []
   (println "hello world"))
     
 (defn init [])
+
+(defn update [tpf])
+
+(defn stop [])
---
+++ app/src/main/java/clj/state.clj
@@ -0,0 +1,4 @@
+(ns clj.state)
+
+(def state (atom {}))
+
